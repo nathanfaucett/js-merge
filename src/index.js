4dbe68b49fb03770a256c8da8fccd3efb6ea67a8
Nathan Faucett <nathanfaucett@gmail.com>
2015-05-01 11:38:40 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ var keys = require("keys"),
     isNullOrUndefined = require("is_null_or_undefined");
 
 
+module.exports = merge;
+
+
+function merge(out) {
+    var i = 0,
+        il = arguments.length - 1;
+
+    while (i++ < il) {
+        baseMerge(out, arguments[i]);
+    }
+
+    return out;
+}
+
 function baseMerge(a, b) {
     var objectKeys = keys(b),
         i = -1,
@@ -16,14 +30,3 @@ function baseMerge(a, b) {
         }
     }
 }
-
-module.exports = function merge(out) {
-    var i = 0,
-        il = arguments.length - 1;
-
-    while (i++ < il) {
-        baseMerge(out, arguments[i]);
-    }
-
-    return out;
-};
